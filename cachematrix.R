7ffea4e650f8b01ceb343ad50838668395818333
mikeShout <mike@two-fish.net>
2015-05-23 12:43:39 -0700

    Updated comments in code and test results in MikeSolution.md

--- cachematrix.R
+++ cachematrix.R
@@ -5,13 +5,15 @@ makeCacheMatrix <- function(aMatrix = matrix()) {
       # initiatie a variable that will return the inversed (solved) matrix
       inversedMatrix <- NULL 
       
-      #The set function clears the variables across the makeCacheMatrix AND parent (cacheSolve) environments...
+      #The set function establishes the initial matrix and NULLS the varaible that is used in 
+      # determining if the answer was already determined (across the makeCacheMatrix AND parent
+      # (cacheSolve) environments...
       set <- function(y) {
             aMatrix <<- y
             inversedMatrix <<- NULL
       }
       
-      # returns the matrix to solve...
+      # returns the supplied matrix (the matrix to solve)...
       get <- function() {
             aMatrix
       }
@@ -30,15 +32,16 @@ makeCacheMatrix <- function(aMatrix = matrix()) {
       list(set = set, get = get, setInvMatrix = setInvMatrix, getInvMatrix = getInvMatrix)
 }
 
-cacheSolve <- function(x){
+cacheSolve <- function(x, ...){
       
       # Get the value of the inversed (solved) matrix. If it does not exist, then the value is NULL,
-      # otherwuise it has a value and thus may have already been solved
+      # otherwise it has a value and thus may have already been solved
       inversedMatrix <- x$getInvMatrix()
       
       # If the value is not NULL, then return this value and exit the function instead of 
       # proceeding to calculate the inversed matrix
       if(!is.null(inversedMatrix)) {
+            # Notify user that a re-calculation is not nexessary - useful for testing
             message("There is a cached solution, so not re-running the calculation...")
             return(inversedMatrix)
       }
@@ -46,7 +49,7 @@ cacheSolve <- function(x){
       # continuing to the steps below if the solved matrix is NULL...
       
       # Supply the matrix to be solved to the solve function using the get() funtcion...
-      inversedMatrix <- solve(x$get())
+      inversedMatrix <- solve(x$get(), ...)
       
       # assign the solution matrix to the inversedMatrix varaible so that is it no longer NULL
       # This will enable the next iteration to skip the calculation step 
